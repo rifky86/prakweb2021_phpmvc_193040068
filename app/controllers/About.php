35fa2b30123c286a36cb1ac830931a2f04fdff6e
rifky86 <goblogpayah123@gmail.com>
2021-10-02 02:39:28 +0700

    controller aplikasi MVC dengan PHP(tugas4)

---
+++ app/controllers/About.php
@@ -0,0 +1,13 @@
+<?php
+
+class About{
+
+    public function index($nama,  $pekerjaan)
+    {
+        echo "Hallo nama saya $nama, saya adalah seorang $pekerjaan";
+    }
+    public function page()
+    {
+        echo 'about/page';
+    }
+}
