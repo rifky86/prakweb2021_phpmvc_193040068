ba80098795fc7a5460e5ad92a56702a2fa4f289b
rifky86 <goblogpayah123@gmail.com>
2021-10-10 00:49:53 +0700

    Asset aplikasi MVC dengan PHP(tugas4)

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,5 @@
-
-    <h1>About Me</h1>
-    <p>Hallo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?>, saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+<div class="container">
+  <h1 class="mt-4">About Me</h1>
+  <img src="<?= BASEURL; ?>/img/IMG_20190222_104422~2.jpg" alt="Rifky Maulana" width="150" class="rounded-circle shadow">
+  <p>Hallo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?>, saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+</div> 
